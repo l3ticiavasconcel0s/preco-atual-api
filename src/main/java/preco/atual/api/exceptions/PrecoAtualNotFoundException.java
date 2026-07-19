5d1174b46eb6acc3d4f3f61ba3c45f2a0aa148d7
Vitória Bentes <73360746+vitoriabentes@users.noreply.github.com>
2026-07-19 14:39:52 -0300

    Atualizando repositório

--- src/main/java/preco/atual/api/exceptions/PrecoAtualNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package preco.atual.api.exceptions;
-
-public class PrecoAtualNotFoundException extends RuntimeException {
-
-	public PrecoAtualNotFoundException(String codigoAtivo) {
-		super("Nenhum preço atual encontrado para o ativo: " + codigoAtivo);
-	}
-}
